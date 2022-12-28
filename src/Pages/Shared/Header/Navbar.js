80c2a6dafed5706c1f7513d821a18ca22d0fa570
partho2589 <parthomalo615@.com>
2022-12-28 23:43:54 +0600

    added footer data

--- src/Pages/Shared/Header/Navbar.js
+++ src/Pages/Shared/Header/Navbar.js
@@ -5,7 +5,8 @@ const  Navbar = () => {
     return (
         <div>
        <Link to='/'>Home</Link>    
-       <Link to='/about'>About</Link>    
+       <Link to='/about'>About</Link>
+       <button class="btn btn-primary">Button</button>    
         </div>
     );
 };
