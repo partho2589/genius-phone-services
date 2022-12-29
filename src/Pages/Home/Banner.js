f36a9655a558baacfeba22e97d0de9acdabad266
partho2589 <parthomalo615@.com>
2022-12-29 16:00:01 +0600

    added banner data

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../Images/banner.jpg'
+const Banner = () => {
+    return (
+        <div>
+          <img className='w-full my-8' src={banner} alt="" />
+
+        </div>
+    );
+};
+
+export default Banner;
