a7b6d91f2ce9aed70f3bd521aadc19f637ce0930
partho2589 <parthomalo615@.com>
2022-12-31 00:39:00 +0600

    add service data

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 const Service = ({ service }) => {
+    console.log(service)
+    const {name, img,description,price} = service;
     return (
         <div>
-            <h1>Name: {service.name}</h1>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img className='w-100 h-auto' src={img} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title">Name: {name}</h2>
+                    <h2 className="card-title">Price: {price}</h2>
+                    <p>Description: {description}</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,36 +1,46 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
 const Services = () => {
- const services =[
-    {
-        _id: 1,
-        name: 'Fluoride Treatment',
-        description: '',
-        img: ''
-    },
-    {
-        _id: 2,
-        name: 'Cavity Filling',
-        description: '',
-        img: ''
-    },
-    {
-        _id: 3,
-        name: 'Teeth Whitening',
-        description: '',
-        img: ''
-    },
-];
+    //  const services =[
+    //     {
+    //         _id: 1,
+    //         name: 'Fluoride Treatment',
+    //         description: '',
+    //         img: ''
+    //     },
+    //     {
+    //         _id: 2,
+    //         name: 'Cavity Filling',
+    //         description: '',
+    //         img: ''
+    //     },
+    //     {
+    //         _id: 3,
+    //         name: 'Teeth Whitening',
+    //         description: '',
+    //         img: ''
+    //     },
+    // ];
+    const [services, setServices] = useState()
+    useEffect(() => {
+        fetch("http://localhost:5000/product")
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
     return (
         <div>
-            <div><h1 className='text-3xl  font-bold text-cyan-400 text-center mt-4 mb-3'>Our services </h1></div>
-            <h1>This is service </h1>
-           {
-            services.map(service => <Service
-            key={service._id}
-            service = {service}
-            ></Service>)
-           }
+            <h1 className='text-3xl  font-bold text-cyan-400 text-center mt-4 mb-3'>Our services </h1>
+
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+
         </div>
     );
 };
