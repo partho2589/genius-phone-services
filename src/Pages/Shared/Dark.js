c7cf41f2ff0da51aa7d6a699dfaf9682af5b7599
partho2589 <parthomalo615@.com>
2023-02-16 19:44:31 +0600

    added data

---
+++ src/Pages/Shared/Dark.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Button, Container, InputGroup } from 'reactstrap';
+import { ThemeContext, themes } from './contexts/ThemeContext';
+import ToggleDark from './components/toggleDark';
+function Dark() {
+    const [darkMode, setDarkMode] = React.useState(true);
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-warning">Dark/Light mode</h1>
+          <InputGroup>
+            <ThemeContext.Consumer>
+              {({ changeTheme }) => (
+                <Button
+                  color="link"
+                  onClick={() => {
+                    setDarkMode(!darkMode);
+                    changeTheme(darkMode ? themes.light : themes.dark);
+                  }}
+                >
+                  <i className={darkMode ? 'fas fa-sun' : 'fas fa-moon'}></i>
+                  <span className="d-lg-none d-md-block">Switch mode</span>
+                </Button>
+              )}
+            </ThemeContext.Consumer>
+          </InputGroup>
+        </header>
+      </div>
+    );
+  }
+
+export default Dark;
