b2cdbb46f1026cad8bbfc24163bc9e3caf119bae
partho2589 <parthomalo615@.com>
2023-03-03 00:03:48 +0600

    added login and register page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from './Pages/Home/Navbar';
 import Services from './Pages/Home/Services';
 import Footer from './Pages/Shared/Footer/Footer';
 import Login from './Pages/Shared/Login/Login';
+import Register from './Pages/Shared/Register';
 function App() {
   return (
     <div className='App '>
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/services' element={<Services/>}></Route>
       </Routes>
       <Footer/>
--- src/Pages/Home/Navbar.js
+++ src/Pages/Home/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex="0" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             <li> <Link to='/'>Home</Link></li>
@@ -18,6 +18,7 @@ const Navbar = () => {
                                 <Link to="/about" className="justify-between"> About</Link >
                             </li>
                             <li><Link to="/login">Login</Link></li>
+                            <li><Link to= "/register">Register</Link></li>
                         </ul>
                     </div>
                     <Link to='/' className="btn btn-ghost normal-case text-xl" >Phone Service</Link>
@@ -30,6 +31,7 @@ const Navbar = () => {
                             <Link to='about'>About </Link>
                         </li>
                         <li><Link to="/login">Login</Link></li>
+                        <li><Link to="/register">Register</Link></li>
                     </ul>
                 </div>
             </div>
